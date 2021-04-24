3f07d74dea12266654b672ee1e641c884e7fe78e
jessijoke <jessijoke@yahoo.com>
2021-04-24 16:22:46 -0400

    bubbles bob

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,87 +1,63 @@
-numBalls = 7;
-const balls = [];
+let bubbleContainer = document.getElementById("bubble-container");
 
-function initializeBubbles() {
-    for (let i = 0; i < numBalls; i++) {
-        createBall();
+class Bubble {
+    constructor(containingDiv) {
+        let bubble = document.createElement("ball");
+        bubble.classList.add("ball");
+        bubble.style.left = `${Math.floor(Math.random() * 100)}vw`;
+        bubble.style.top = `${Math.floor(Math.random() * 100)}vh`;
+        bubble.style.transform = `scale(${Math.random()})`;
+        bubble.style.width = `${(Math.floor(Math.random() * 30) + 5)}em`;
+        bubble.style.height = bubble.style.width;
+        containingDiv.append(bubble);
+
+        let to = {
+            x: Math.random() * (1 % 2 === 0 ? -11 : (Math.random() * 40)),
+            y: Math.random() * (Math.random() * 40)
+          };
+        
+          let anim = bubble.animate(
+            [
+              { transform: "translate(0, 0)" },
+              { transform: `translate(${to.x}rem, ${to.y}rem)` }
+            ],
+            {
+              duration: (Math.random() + 5) * 2000, 
+              direction: "alternate",
+              fill: "both",
+              iterations: Infinity,
+              easing: "ease-in-out"
+            }
+          );
+
+          bubble.addEventListener('click', (e)=> {
+            console.log("click");
+            bubble.removeEventListener('click', (e));
+            bubble.remove();
+            bubbles[bubbles.length] =  new Bubble(bubbleContainer);
+            bubbles.push(bubbles[bubbles.length]);
+          });
+          /*
+          $('.ball').ripples({
+              resolution: 512,
+              dropRadius: 50,
+              perturbance: 0.02,
+          });
+          */
     }
+
 }
 
-initializeBubbles(); 
-
-function createBall() {
-  let ball = document.createElement("ball");
-  ball.classList.add("ball");
-  ball.style.left = `${Math.floor(Math.random() * 80)}vw`;
-  ball.style.top = `${Math.floor(Math.random() * 40)}vh`;
-  ball.style.transform = `scale(${Math.random()})`
-  ball.style.width = `${(Math.floor(Math.random() * 30) + 5)}em`;
-  ball.style.height = ball.style.width;
-  
-  balls.push(ball);
-  document.body.append(ball);
-  
-  let to = {
-    x: Math.random() * (1 % 2 === 0 ? -11 : 22),
-    y: Math.random() * 24
-  };
-
-  let anim = ball.animate(
-    [
-      { transform: "translate(0, 0)" },
-      { transform: `translate(${to.x}rem, ${to.y}rem)` }
-    ],
-    {
-      duration: (Math.random() + 2) * 2000, 
-      direction: "alternate",
-      fill: "both",
-      iterations: Infinity,
-      easing: "ease-in-out"
-    }
-  );
+let bubbles = []
+for (let i = 0; i < 11; i++) {
+    bubbles[i] = new Bubble(bubbleContainer);
+    bubbles.push(bubbles[i]);
 }
 
-let theBall = document.querySelectorAll("ball");
-
-theBall[1].addEventListener("mouseover", moveBall);
-
-function moveBall() {
-    console.log('BALLLLLLL');
-    let to = {
-        x: Math.random() * (1 % 2 === 0 ? -11 : 22),
-        y: Math.random() * 24
-      };
-    let anim = theBall[1].animate(
-        [
-          { transform: "translate(0, 0)" },
-          { transform: `translate(${to.x}rem, ${to.y}rem)` }
-        ],
-        {
-          duration: (Math.random() + 2) * 2000, 
-          direction: "alternate",
-          fill: "both",
-          iterations: Infinity,
-          easing: "ease-in-out"
-        }
-      );
-}
-
-
-let bubbles = document.querySelectorAll("ball");
-let bubbleCount = bubbles.length - 1;
-
-for (let i = 0; i < bubbles.length; i++) {
-    addNewBubbleEvent(bubbles[i]);
-}
-
-function addNewBubbleEvent(bubble) {
-    bubble.addEventListener('click', (e)=> {
-        console.log("click");
-        this.removeEventListener('click', (e));
-        bub = bubble;
-        bub.remove();
-        createBall();
-        //bubbleCount += 1;
-        //addNewBubbleEvent(bubbles[bubbleCount]);
-    });
-}
+/*
+$('.container').ripples({
+    resolution: 512,
+    dropRadius: 50,
+    perturbance: 0.02,
+});
+*/
